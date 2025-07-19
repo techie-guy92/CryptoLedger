1a514ca3c584e16153ceec3403c295d299276d3e
techie-guy92 <soheil.dalirii@gmail.com>
2025-07-19 10:44:48 +0330

    urls were set up

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include('main.urls')),
+    path('api-auth', include('rest_framework.urls')),
+    path("api-schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api-documents/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_title = 'CryptoLedger'
+admin.site.index_title = 'CryptoLedger'
+admin.site.site_header = 'CryptoLedger Administration'
