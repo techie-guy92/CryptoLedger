482dd9b1d82e0ac90f11982d5a0888c26433b30f
techie-guy92 <soheil.dalirii@gmail.com>
2025-07-19 14:07:41 +0330

    users app is created

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
+    path('users/', include('users.urls')),
     path('api-auth', include('rest_framework.urls')),
     path("api-schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api-documents/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
