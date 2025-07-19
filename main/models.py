a68135f83eaee5e690d38c3743b9196933320254
techie-guy92 <soheil.dalirii@gmail.com>
2025-07-19 12:40:27 +0330

    completed models

--- main/models.py
+++ main/models.py
@@ -19,22 +19,74 @@ def upload_to(instance, filename):
 class TargetCoin(models.Model):
     SIGNIFICANCE = [("1", "*"), ("2", "**"), ("3", "***"), ("4", "****"), ("5", "*****"),]
     coin = models.CharField(max_length=20, verbose_name="Coin")
-    min_target_price = models.IntegerField(default=0, verbose_name="Min Price")
-    max_target_price = models.IntegerField(default=0, verbose_name="Max Price")
+    min_target_price = models.DecimalField(max_digits=13, decimal_places=8, verbose_name="Min Price")
+    max_target_price = models.DecimalField(max_digits=13, decimal_places=8, verbose_name="Max Price")
     significance = models.CharField(max_length=10, choices=SIGNIFICANCE, verbose_name="Significance")
+    signal_notes = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Created At")
+
+    def __str__(self):
+        return f"{self.coin} at ({self.min_target_price} and {self.max_target_price})"
+
+    class Meta:
+        verbose_name = "Target Coin"
+        verbose_name_plural = "Target Coins"
+        indexes = [
+            models.Index(fields=["coin"]),
+            models.Index(fields=["significance"]),
+        ]
 
 
 #====================================== BoughtCoin Model ==============================================
 
 class BoughtCoin(models.Model):
     coin = models.CharField(max_length=20, verbose_name="Coin")
-    bought_price = models.IntegerField(default=0, verbose_name="Min Price")
-    sold_price = models.IntegerField(default=0, verbose_name="Min Price")
+    bought_price = models.DecimalField(max_digits=13, decimal_places=8, verbose_name="Min Price")
+    sold_price = models.DecimalField(max_digits=13, decimal_places=8, verbose_name="Min Price")
     is_available = models.BooleanField(default=True, verbose_name="Is Available")
-    profit = models.IntegerField(verbose_name="Profit")
+    profit = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Profit (%)")
     bought_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Bought At")
-    sold_at = models.DateTimeField(editable=False, verbose_name="Sold At")
+    sold_at = models.DateTimeField(verbose_name="Sold At")
 
+    def __str__(self):
+        return f"{self.coin} bought at {self.bought_price} and sold at {self.sold_price}"
+        
+    def save(self, *args, **kwargs):
+        if self.sold_price:
+            self.is_available = False
+        if self.sold_price and self.bought_price:
+            change = (self.sold_price - self.bought_price) / self.bought_price
+            self.profit = change * 100
+        super().save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = "Bought Coin"
+        verbose_name_plural = "Bought Coins"
+        indexes = [
+            models.Index(fields=["coin"]),
+            models.Index(fields=["is_available"]),
+        ]
+        
+        
+#====================================== BoughtCoin Model ==============================================
+        
+class MostBoughtCoin(models.Model):
+    SOURCE = [("1", "TradingView"), ("2", "CoinMarketCap"), ("3", "Cryptometer")]
+    coin = models.CharField(max_length=20, verbose_name="Coin")
+    rank = models.IntegerField(blank=True, null=True, verbose_name="Rank")
+    source = models.CharField(max_length=20, choices=SOURCE, verbose_name="Data Source")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Created At")
+    
+    def __str__(self):
+        return f"{self.coin} at {self.created_at}"
+    
+    class Meta:
+        verbose_name = "Most Bought Coin"
+        verbose_name_plural = "Most Bought Coins"
+        indexes = [
+            models.Index(fields=["coin"]),
+            models.Index(fields=["created_at"]),
+        ]
+        
+        
 #======================================================================================================
