1799d1f1ca99742de4f09850802e2fbb623ad3c1
techie-guy92 <soheil.dalirii@gmail.com>
2025-07-22 17:28:30 +0330

    update for bat file

--- run_cryptoledger.bat
+++ run_cryptoledger.bat
@@ -3,8 +3,9 @@ title Starting CryptoLedger Local Server...
 cd /d "%~dp0"
 echo [%DATE% %TIME%] Starting CryptoLedger >> startup_log.txt
 call venv\Scripts\activate
+echo [%DATE% %TIME%] Virtualenv activated >> startup_log.txt
 timeout /t 2 >nul
 python manage.py runserver 8020 >> startup_log.txt 2>&1
 echo [%DATE% %TIME%] Finished runserver >> startup_log.txt
-pause
 start http://localhost:8020
+echo [%DATE% %TIME%] CryptoLedger boot sequence complete >> startup_log.txt
