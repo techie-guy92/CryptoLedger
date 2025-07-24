4347a71cbe30563736931f8038bdc6e4b2d41fbd
techie-guy92 <soheil.dalirii@gmail.com>
2025-07-24 10:28:53 +0330

    log file is moved

--- run_cryptoledger.bat
+++ run_cryptoledger.bat
@@ -1,11 +1,11 @@
 @echo on
 title Starting CryptoLedger Local Server...
 cd /d "%~dp0"
-echo [%DATE% %TIME%] Starting CryptoLedger >> startup_log.txt
+echo [%DATE% %TIME%] Starting CryptoLedger >> logs\startup_log.txt
 call venv\Scripts\activate
-echo [%DATE% %TIME%] Virtualenv activated >> startup_log.txt
+echo [%DATE% %TIME%] Virtualenv activated >> logs\startup_log.txt
 timeout /t 2 >nul
-python manage.py runserver 8020 >> startup_log.txt 2>&1
-echo [%DATE% %TIME%] Finished runserver >> startup_log.txt
+python manage.py runserver 8020 >> logs\startup_log.txt 2>&1
+echo [%DATE% %TIME%] Finished runserver >> logs\startup_log.txt
 start http://localhost:8020
-echo [%DATE% %TIME%] CryptoLedger boot sequence complete >> startup_log.txt
+echo [%DATE% %TIME%] CryptoLedger boot sequence complete >> logs\startup_log.txt
--- start_cryptoledger.sh
+++ start_cryptoledger.sh
@@ -1,9 +1,9 @@
-echo "[`date '+%Y-%m-%d %H:%M:%S'`] Starting CryptoLedger" >> startup_log.txt
 cd "$(dirname "$0")"
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Starting CryptoLedger" >> logs/startup_log.txt
 source venv/bin/activate
-echo "[`date '+%Y-%m-%d %H:%M:%S'`] Virtualenv activated" >> startup_log.txt
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Virtualenv activated" >> logs/startup_log.txt
 sleep 2
-python manage.py runserver 8020 >> startup_log.txt 2>&1 &
-echo "[`date '+%Y-%m-%d %H:%M:%S'`] Finished runserver" >> startup_log.txt
+python manage.py runserver 8020 >> logs/startup_log.txt 2>&1 &
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Finished runserver" >> logs/startup_log.txt
 xdg-open http://localhost:8020
-echo "[`date '+%Y-%m-%d %H:%M:%S'`] CryptoLedger boot sequence complete" >> startup_log.txt
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] CryptoLedger boot sequence complete" >> logs/startup_log.txt
