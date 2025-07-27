84c3d61d25f24dafe29994a1ddab170dabef824e
techie-guy92 <soheil.dalirii@gmail.com>
2025-07-27 09:42:24 +0330

    update batch file in case of facing clock drift

--- run_cryptoledger.bat
+++ run_cryptoledger.bat
@@ -1,11 +1,23 @@
-@echo on
+@echo off
 title Starting CryptoLedger Local Server...
 cd /d "%~dp0"
-echo [%DATE% %TIME%] Starting CryptoLedger >> logs\startup_log.txt
+
+echo [STARTUP INIT] [%DATE% %TIME%] Boot script triggered >> logs\startup_log.txt
+
+:: Sync system time before launch (protects against drift)
+timeout /t 3 >nul
+w32tm /resync
+echo [TIME SYNC] [%DATE% %TIME%] System time resynced >> logs\startup_log.txt
+timeout /t 3 >nul
+
+echo [POST-SYNC TIME] %DATE% %TIME% >> logs\startup_log.txt
+
 call venv\Scripts\activate
-echo [%DATE% %TIME%] Virtualenv activated >> logs\startup_log.txt
-timeout /t 2 >nul
+echo [ENV] [%DATE% %TIME%] Virtualenv activated >> logs\startup_log.txt
+
+timeout /t 10 >nul
 python manage.py runserver 8020 >> logs\startup_log.txt 2>&1
-echo [%DATE% %TIME%] Finished runserver >> logs\startup_log.txt
+echo [DJANGO] [%DATE% %TIME%] Finished runserver >> logs\startup_log.txt
+
 start http://localhost:8020
-echo [%DATE% %TIME%] CryptoLedger boot sequence complete >> logs\startup_log.txt
+echo [COMPLETE] [%DATE% %TIME%] CryptoLedger boot sequence complete >> logs\startup_log.txt
