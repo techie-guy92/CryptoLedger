41cd39de1d531913133358288269b616eaee61cb
techie-guy92 <soheil.dalirii@gmail.com>
2025-08-02 12:39:45 +0330

    added new module to fetch usdt and ounce prices

---
+++ tgju_utils.py
@@ -0,0 +1,17 @@
+import aiohttp
+import asyncio
+import json
+import logging
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+logger = logging.getLogger(__name__)
+
+#====================================== fetch usdt & ounce prices ======================================
+
+async def fetch_usdt_and_ounce_prices():
+    pass
+
+
+#=======================================================================================================
