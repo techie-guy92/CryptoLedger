e80905922beabf3ba1798ef5283092d4356ee5b9
techie-guy92 <soheil.dalirii@gmail.com>
2026-06-16 22:27:19 +0330

    Add two different health check for probes

--- config/urls.py
+++ config/urls.py
@@ -20,16 +20,64 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.db import connection
+from django.db.utils import OperationalError
 
-def health_check(request):
-    return JsonResponse({"status": "healthy", "service": "crypto-ledger"})
+
+# def health_check(request):
+#     try:
+#         connection.ensure_connection()
+#         return JsonResponse({
+#             "status": "healthy",
+#             "database": "connected"
+#         })
+#     except OperationalError:
+#         return JsonResponse(
+#             {
+#                 "status": "unhealthy",
+#                 "database": "disconnected"
+#             },
+#             status=503
+#         )
+
+
+def live_check(request):
+    """
+    Is Django process alive?
+    """
+    return JsonResponse({
+        "status": "alive"
+    })
+
+
+def ready_check(request):
+    """
+    Can this pod serve traffic?
+    """
+    try:
+        connection.ensure_connection()
+        return JsonResponse({
+            "status": "ready",
+            "database": "connected"
+        })
+    except OperationalError:
+        return JsonResponse(
+            {
+                "status": "not ready",
+                "database": "disconnected"
+            },
+            status=503
+        )
+
 
 urlpatterns = [
-    path('health/', health_check, name='health-check'),
+    # path('health/', health_check, name='health-check'),
+    path("health/live/", live_check, name="live-check"),
+    path("health/ready/", ready_check, name="ready-check"),
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
     path('users/', include('users.urls')),
-    path('api-auth', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path("api-schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api-documents/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
