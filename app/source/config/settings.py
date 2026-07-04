a43b7e4a36f9d1f11acf03f39f3fad5aa6b61584
techie-guy92 <soheil.dalirii@gmail.com>
2026-07-04 19:45:05 +0330

    add STORAGES in settings.py for whitenoise

--- app/source/config/settings.py
+++ app/source/config/settings.py
@@ -167,6 +167,15 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = path.join(BASE_DIR, 'media') 
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
